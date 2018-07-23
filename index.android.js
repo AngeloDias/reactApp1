f30b389928db16599797eeaf7230e3f887578b47
angelossdias <D14sAng>
2018-07-23 14:56:11 -0300

    Arrow functions, multi elementos e atribuição desestruturada

--- index.android.js
+++ index.android.js
@@ -1,11 +1,23 @@
 var React = require("react");
-var Text = require("react-native").Text;
-var AppRegistry = require("react-native").AppRegistry;
+/*var Text = require("react-native").Text;
+var View = require("react-native").View;
+var Button = require("react-native").Button;
+var AppRegistry = require("react-native").AppRegistry;*/
 
-const App = function(){
+var {Text, View, Button, AppRegistry} = require("react-native");
+
+const geraNumRand = () => {
+	alert (Math.floor(Math.random() * 10));
+}
+
+const App = () => {
   return (
-    <Text>Meu primeiro APP</Text>
+    /* O elemento View deve conter outros elementos, ele é o principal. */
+    <View>
+    	<Text>Gerador de números randômicos</Text>
+    	<Button title="Gerar número randômico" onPress={geraNumRand} />
+    </View>
   );
 };
 
-AppRegistry.registerComponent("reactApp1", function() {return App});
+AppRegistry.registerComponent("reactApp1", () => App);
